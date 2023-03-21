0d5b1389c923c1b71ba84f1de1c31b66b45e3d7b
nx006 <limazero14@gmail.com>
2023-03-21 20:39:49 +0900

    Refactor: Add inline keyword to getter function

--- nx006/week3/12865번/12865.cpp
+++ nx006/week3/12865번/12865.cpp
@@ -5,91 +5,108 @@
 class Item
 {
 public:
-  Item(const int value, const int weight) : value(value), weight(weight) {}
-  int getValue() const { return value; }
-  int getWeight() const { return weight; }
+    Item(const int value, const int weight) : value(value), weight(weight) {}
+    inline int getValue() const { return value; }
+    inline int getWeight() const { return weight; }
+
 private:
-  int value;
-  int weight;
+    int value;
+    int weight;
 };
 
-class DPManager {
+class DPManager
+{
 public:
-  DPManager(const std::size_t numberOfItems, const int maxWeight) : numberOfItems(numberOfItems), maxWeight(maxWeight) {
-    dp.resize(numberOfItems + 1);
-    for (auto& row : dp) {
-      row.resize(maxWeight + 1);
-    }
-
-    items.reserve(numberOfItems);
-  }
-
-  auto inputItems() {
-    items.emplace_back(0, 0); // index 0 -> empty item
-    for (std::size_t i = 0; i < numberOfItems; i++) {
-      int weight = 0;
-      int value = 0;
-      std::cin >> weight >> value;
-      items.emplace_back(value, weight);
-    }
-  }
-
-  auto displayDp() const -> void {
-    for (const auto& row : dp) {
-      for (const auto value : row) {
-        std::cout << value << ' ';
-      }
-      std::cout << std::endl;
-    }
-  }
-
-  void calculateMaxValue() {
-    knapsack_dp();
-  }
-
-  auto getMaxValue() {
-    return dp.back().back();
-  }
-private:
-  std::vector< std::vector<int> > dp;
-  std::vector< Item > items;
-
-  int maxWeight;
-  std::size_t numberOfItems;
-
-  void knapsack_dp() {
-    // itemIndex stands for column index
-    for (int itemIndex = 1; itemIndex < items.size(); itemIndex++) {
-      // weightIndex stands for row index
-      for (int weightIndex = 1; weightIndex < maxWeight + 1; weightIndex++) {
-        // for case of bad access
-        if (weightIndex - items[itemIndex].getWeight() < 0) {
-          dp[itemIndex][weightIndex] = dp[itemIndex - 1][weightIndex];
-          continue;
+    DPManager(const std::size_t numberOfItems, const int maxWeight)
+        : numberOfItems(numberOfItems),
+          maxWeight(maxWeight)
+    {
+        dp.resize(numberOfItems + 1);
+        for (auto &row : dp)
+        {
+            row.resize(maxWeight + 1);
         }
-        dp[itemIndex][weightIndex] = std::max(
-            dp[itemIndex - 1][weightIndex], // NS(n-1, w) + 0
-            dp[itemIndex - 1][weightIndex - items[itemIndex].getWeight()]
-                + items[itemIndex].getValue() // NS(n-1, w - w[n]) + v[n]
-        );
-      }
+
+        items.reserve(numberOfItems);
     }
-  }
+
+    auto inputItems()
+    {
+        items.emplace_back(0, 0); // index 0 -> empty item
+        for (std::size_t i = 0; i < numberOfItems; i++)
+        {
+            int weight = 0;
+            int value = 0;
+            std::cin >> weight >> value;
+            items.emplace_back(value, weight);
+        }
+    }
+
+    auto displayDp() const -> void
+    {
+        for (const auto &row : dp)
+        {
+            for (const auto value : row)
+            {
+                std::cout << value << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void calculateMaxValue()
+    {
+        knapsack_dp();
+    }
+
+    auto getMaxValue()
+    {
+        return dp.back().back();
+    }
+
+private:
+    std::vector<std::vector<int>> dp;
+    std::vector<Item> items;
+
+    int maxWeight;
+    std::size_t numberOfItems;
+
+    void knapsack_dp()
+    {
+        // itemIndex stands for column index
+        for (int itemIndex = 1; itemIndex < items.size(); itemIndex++)
+        {
+            // weightIndex stands for row index
+            for (int weightIndex = 1; weightIndex < maxWeight + 1; weightIndex++)
+            {
+                // for case of bad access
+                if (weightIndex - items[itemIndex].getWeight() < 0)
+                {
+                    dp[itemIndex][weightIndex] = dp[itemIndex - 1][weightIndex];
+                    continue;
+                }
+                dp[itemIndex][weightIndex] = std::max(
+                    dp[itemIndex - 1][weightIndex],                                                             // NS(n-1, w) + 0
+                    dp[itemIndex - 1][weightIndex - items[itemIndex].getWeight()] + items[itemIndex].getValue() // NS(n-1, w - w[n]) + v[n]
+                );
+            }
+        }
+    }
 };
 
 int main()
 {
-  std::cin.tie(nullptr)->sync_with_stdio(false);
-  std::cout.tie(nullptr)->sync_with_stdio(false);
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cout.tie(nullptr)->sync_with_stdio(false);
 
-  int numberOfItems = 0;
-  int maximumWeight = 0;
-  std::cin >> numberOfItems >> maximumWeight;
-  DPManager kn(numberOfItems, maximumWeight);
-  kn.inputItems();
-  kn.calculateMaxValue();
+    int numberOfItems = 0;
+    int maximumWeight = 0;
+    std::cin >> numberOfItems >> maximumWeight;
+    DPManager kn(numberOfItems, maximumWeight);
+    kn.inputItems();
+    kn.calculateMaxValue();
 
-  std::cout << kn.getMaxValue() << std::endl;
+    std::cout << kn.getMaxValue() << std::endl;
 
-  return 0;
+    return 0;
 }
